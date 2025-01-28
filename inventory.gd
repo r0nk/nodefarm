434bc5a48fada04c64522f06b53acb84c9216efb
r0nk <r0nk@simplecpu.com>
2025-01-27 20:34:06 -0600

    drag and droppable items.

--- inventory.gd
+++ inventory.gd
@@ -5,13 +5,18 @@ signal item_picked_up(item)
 func add(thing:Item):
 	var slots = get_tree().get_nodes_in_group("slot")
 	slots.sort()
+	slots.reverse()
 	slots = slots.filter(func(s):return not s.is_in_group("equipment_slot"))
 	#slots.map(print)
 	for slot in slots:
-		if not slot.item:
-			slot.item=thing
-			break
+		if(!slot.item):
+			continue
 		if(slot.item.id==thing.id and (slot.item.count<slot.item.max_stacks)):
 			slot.item.count+=1
-			break
-	item_picked_up.emit(thing)
+			item_picked_up.emit(thing)
+			return
+	for slot in slots:
+		if not slot.item:
+			slot.item=thing
+			item_picked_up.emit(thing)
+			return
--- item_slot.gd
+++ item_slot.gd
@@ -2,8 +2,13 @@ extends Button
 
 @export var item:Item
 
+var origin
+var dragging=false
+var hovering=false
+
 func hover_on():
-	if not item:
+	hovering=true
+	if not item or dragging:
 		$tooltip.visible=false
 		return
 	$tooltip/title.text=item.id
@@ -12,13 +17,32 @@ func hover_on():
 	$tooltip.visible=true
 
 func hover_off():
+	hovering=false
 	$tooltip.visible=false
 
+func start_drag():
+	dragging=true
+	flat=dragging
+
+func stop_drag():
+	dragging=false
+	flat=dragging
+	position=origin
+	var m = get_global_mouse_position()
+	var slots = get_tree().get_nodes_in_group("slot")
+	slots = slots.filter(func(s):return s.hovering)
+	var buf = item
+	item=slots[0].item
+	slots[0].item=buf
+
 func _ready():
+	origin=position
 	mouse_entered.connect(hover_on)
 	mouse_exited.connect(hover_off)
 
 func _process(_delta):
+	if(dragging):
+		global_position=get_global_mouse_position()
 	if not item:
 		$count.text=""
 		icon=null
