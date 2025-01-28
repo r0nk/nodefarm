3bf260444f60dd9be5a0c29561474f766d1a8de9
r0nk <r0nk@simplecpu.com>
2025-01-27 19:48:30 -0600

    adding item directly to item slot.

--- inventory.gd
+++ inventory.gd
@@ -1,29 +1,17 @@
 extends Node3D
 
-@export var slots = {"one":null,"two":null,"three":null}
-
-@export var max_slots = 9
-
-@export var filled_slots = 0
-
 signal item_picked_up(item)
 
 func add(thing:Item):
-	for key in slots.keys():
-		if not slots[key]:
-			slots[key]=thing
+	var slots = get_tree().get_nodes_in_group("slot")
+	slots.sort()
+	slots = slots.filter(func(s):return not s.is_in_group("equipment_slot"))
+	#slots.map(print)
+	for slot in slots:
+		if not slot.item:
+			slot.item=thing
 			break
-		if(slots[key].id==thing.id and (slots[key].count<slots[key].max_stacks)):
-			slots[key].count+=1
+		if(slot.item.id==thing.id and (slot.item.count<slot.item.max_stacks)):
+			slot.item.count+=1
 			break
 	item_picked_up.emit(thing)
-
-func update_view():
-	if not $view.visible:
-		return
-	for s in $view/hotbar.get_children():
-		if(s.is_in_group("slot")):
-			s.item=slots[s.name]
-
-func _process(delta):
-	update_view()
