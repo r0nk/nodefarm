242177a009047df8e15da657f9acb727d8a9bd77
r0nk <r0nk@simplecpu.com>
2025-06-15 18:42:31 -0500

    store items actually going toward player

--- addons/fpsrpg_controller/inventory.gd
+++ addons/fpsrpg_controller/inventory.gd
@@ -2,8 +2,15 @@ extends Node3D
 
 signal item_picked_up(item)
 
+func get_slots():
+	var c = get_node("../game_menu/Inventory").get_children()
+	var ret = c.filter(func(n):return not n.is_in_group("equipment_slot"))
+	return ret
+
+
 func add(thing:Item):
-	var slots = get_tree().get_nodes_in_group("slot")
+	thing = thing.duplicate()
+	var slots = get_slots()
 	slots.sort()
 	slots.reverse()
 	slots = slots.filter(func(s):return not s.is_in_group("equipment_slot"))
