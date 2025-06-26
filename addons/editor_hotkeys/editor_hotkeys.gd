64ccfdab1f3c4cbb51f69e08f2acc4757d5ea83d
r0nk <r0nk@simplecpu.com>
2025-06-25 20:56:22 -0500

    start of new editor hotkeys.

---
+++ addons/editor_hotkeys/editor_hotkeys.gd
@@ -0,0 +1,21 @@
+@tool
+extends EditorPlugin
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_M and event.ctrl_pressed:
+			print("HOTKEY")
+			var player = get_tree().get_nodes_in_group("player")[0]
+			EditorInterface.edit_node(player)
+		print(event.as_text())
+
+func selection_changed():
+	pass
+
+func _enter_tree() -> void:
+	print("Editor hotkeys entered tree.")
+#	EditorInterface.get_selection().selection_changed.connect(selection_changed)
+
+func _exit_tree() -> void:
+	# Clean-up of the plugin goes here.
+	pass
